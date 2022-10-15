0dcf8d6c8b56048817f66a6d4269983a6f4d1399
koskakos <fafsdfas93@mail.ru>
2022-10-15 15:23:58 +0600

    project created

---
+++ src/main/java/project/weblibrary/WeblibraryApplication.java
@@ -0,0 +1,13 @@
+package project.weblibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/weblibrary/WeblibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package project.weblibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
